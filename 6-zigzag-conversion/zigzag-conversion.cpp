acd1390215b97fd422c110b6062babf345d85e38
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-02-27 06:40:02 +0500

    Time: 17 ms (20.34%) | Memory: 28.2 MB (7.00%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,38 +1,70 @@
+
 class Solution {
 public:
     string convert(string s, int numRows) {
         if (numRows == 1) return s;
 
         int n = s.length();
-        vector<vector<char>> grid(numRows, vector<char>(n, ' '));
+        vector<string> rows;
+        rows.resize(numRows);
 
-        int i = 0; // index in string
-        int col = 0;
+        int step = 1;
+        int row = 0;
 
-        while (i < n) {
-            // Step 1: go down
-            for (int row = 0; row < numRows && i < n; row++) {
-                grid[row][col] = s[i++];
+        for (int i = 0; i < n; i++) {
+            char arr = s[i];
+            rows[row] = rows[row] + arr;
+
+            if (row == 0) {
+                step = 1;
             }
-            col++;
-
-            // Step 2: go diagonally up
-            for (int row = numRows - 2; row > 0 && i < n; row--) {
-                grid[row][col] = s[i++];
-                col++;
+            else if (row == numRows - 1) {
+                step = -1;
             }
+
+            row = row + step;
         }
 
-        // Step 3: read row by row
         string result = "";
-        for (int row = 0; row < numRows; row++) {
-            for (int c = 0; c < n; c++) {
-                if (grid[row][c] != ' ') {
-                    result += grid[row][c];
-                }
-            }
+        for (int i = 0; i < numRows; i++) {
+            result = result + rows[i];
         }
 
         return result;
     }
 };
+
+
+
+        // if (numRows == 1) return s;
+
+        // int n = s.length();
+        // vector<vector<char>> grid(numRows, vector<char>(n, ' '));
+
+        // int i = 0; 
+        // int col = 0;
+
+        // while (i < n) {
+            
+        //     for (int row = 0; row < numRows && i < n; row++) {
+        //         grid[row][col] = s[i++];
+        //     }
+        //     col++;
+
+            
+        //     for (int row = numRows - 2; row > 0 && i < n; row--) {
+        //         grid[row][col] = s[i++];
+        //         col++;
+        //     }
+        // }
+
+        // string result = "";
+        // for (int row = 0; row < numRows; row++) {
+        //     for (int c = 0; c < n; c++) {
+        //         if (grid[row][c] != ' ') {
+        //             result += grid[row][c];
+        //         }
+        //     }
+        // }
+
+        // return result;
