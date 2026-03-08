32504224b90ef502eb6ba4670dce911849675fcd
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-03-08 20:30:43 +0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (84.44%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -3,29 +3,25 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
         unordered_map<int,int> mp;
-        
-        // count frequency
-        for(int i = 0; i < nums.size(); i++)
+        for(int i=0; i< nums.size(); i++)
         {
             mp[nums[i]]++;
         }
 
         vector<pair<int,int>> v;
-
-        // store hashmap into vector
-        for(auto it : mp)
+        for(auto it:mp)
         {
             v.push_back(it);
         }
 
-        // sort by frequency
-        sort(v.begin(), v.end(), [](pair<int,int> a, pair<int,int> b){
+        sort(v.begin(),v.end(),[](pair<int,int> a, pair<int,int> b){
             return a.second > b.second;
         });
 
+
         vector<int> ans;
 
-        // take top k elements
+       
         for(int i = 0; i < k; i++)
         {
             ans.push_back(v[i].first);
