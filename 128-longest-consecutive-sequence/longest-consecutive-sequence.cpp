9217afc5a72787ed14fa5f81ee71be1dc8fefe10
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-03-11 19:10:53 +0500

    Time: 4 ms (99.19%) | Memory: 60.1 MB (97.43%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -4,13 +4,10 @@ public:
         if(nums.size() == 0) return 0;
 
         sort(nums.begin(), nums.end());
-
         int longest = 1;
         int count = 1;
-
         for(int i = 1; i < nums.size(); i++) {
             if(nums[i] == nums[i-1]) continue;
-
             if(nums[i] == nums[i-1] + 1) {
                 count++;
             } 
@@ -19,7 +16,6 @@ public:
                 count = 1;
             }
         }
-
         return max(longest, count);
     }
 };
