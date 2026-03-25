6572f479ea6670b760da5c31c92187b002d4448a
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-03-25 11:20:00 +0500

    Time: 38 ms (74.20%) | Memory: 85 MB (34.60%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.cpp
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -3,14 +3,10 @@ public:
     int totalFruit(vector<int>& fruits) {
         int n = fruits.size();
         unordered_map<int, int> mp;
-
         int start = 0;
         int maxLen = 0;
-
         for (int end = 0; end < n; end++) {
             mp[fruits[end]]++;
-
-            // shrink window if more than 2 types
             while (mp.size() > 2) {
                 mp[fruits[start]]--;
                 if (mp[fruits[start]] == 0) {
@@ -18,11 +14,8 @@ public:
                 }
                 start++;
             }
-
-            // update answer
             maxLen = max(maxLen, end - start + 1);
         }
-
         return maxLen;
     }
 };
