5f1dd2e24288d6ba793891db2971abea2ed41157
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-03-30 20:35:25 +0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (61.06%) - LeetSync

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int ans=0, mx=0;
+        for(auto c: s){
+            if(c == '(') ans++;
+            else if (c==')') {
+                mx = max(mx, ans);
+                ans--;
+            }
+        }
+        return mx;
+    }
+};
