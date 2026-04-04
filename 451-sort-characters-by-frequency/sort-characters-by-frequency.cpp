31e2ded1c6dad13c889b58eb8894a64df97dd9c6
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-04-05 00:58:23 +0500

    Time: 3 ms (56.01%) | Memory: 11 MB (63.71%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -2,33 +2,25 @@ class Solution {
 public:
     string frequencySort(string s) {
         unordered_map<char, int> freq;
-
-        // Step 1: Count frequency
         for (int i = 0; i < s.size(); i++) {
             freq[s[i]]++;
         }
-
-        // Step 2: Move to vector
-        vector<pair<char, int>> vec;
-        for (auto it = freq.begin(); it != freq.end(); it++) {
+        vector<pair<char,int>> vec;
+        for(auto it=freq.begin(); it!=freq.end(); it++)
+        {
             vec.push_back(*it);
         }
-
-        // Step 3: Sort (simple function instead of lambda)
-        sort(vec.begin(), vec.end(), cmp);
-
-        // Step 4: Build result
-        string result = "";
-        for (int i = 0; i < vec.size(); i++) {
-            for (int j = 0; j < vec[i].second; j++) {
+        sort(vec.begin(),vec.end(),cmp);
+        string result="";
+        for(int i=0 ; i< vec.size() ; i++)
+        {
+            for(int j=0 ; j<vec[i].second ; j++)
+            {
                 result += vec[i].first;
             }
         }
-
         return result;
     }
-
-    // Simple comparator function
     static bool cmp(pair<char, int> a, pair<char, int> b) {
         return a.second > b.second;
     }
