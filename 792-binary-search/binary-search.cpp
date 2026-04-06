cfe63776ee0651c36a061e9553609c68aa2b3c9e
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-04-06 23:25:05 +0500

    Time: 0 ms (100.00%) | Memory: 31.4 MB (45.20%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -3,21 +3,21 @@ public:
     int search(vector<int>& nums, int target) {
         int low = 0;
         int high = nums.size() - 1;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (nums[mid] == target) {
+        while(low <= high)
+        {
+            int mid = low+ (high - low) /2;
+            if(nums[mid] == target)
+            {
                 return mid;
             }
-            else if (nums[mid] > target) {
-                high = mid - 1;
+            else if(nums[mid] > target)
+            {
+                high = mid-1;
             }
-            else {
+            else{
                 low = mid + 1;
             }
         }
-
         return -1;
     }
 };
