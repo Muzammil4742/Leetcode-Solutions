dc7d71f8775b7b3d2f3940436ac49cb53f0ee1b6
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-04-29 22:59:54 +0500

    Time: 0 ms (100.00%) | Memory: 9 MB (38.43%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,16 +1,18 @@
-#include <stack>
-#include <string>
-using namespace std;
 
 class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        for (char ch : s) {
-            if (ch == '(' || ch == '[' || ch == '{') {
+        for(char ch : s)
+        {
+            if(ch =='(' || ch=='[' || ch== '{')
+            {
                 st.push(ch);
-            } else {
-                if (st.empty()) {
+            }
+            else
+            {
+                if(st.empty())
+                {
                     return false;
                 }
                 char top = st.top();
