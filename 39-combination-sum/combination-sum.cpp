4cd5b25955ed6ce405aeb6604cefd0202ae3eae0
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-05-25 16:07:53 +0500

    Time: 7 ms (24.25%) | Memory: 16.4 MB (28.53%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,26 +1,58 @@
 class Solution {
 public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> comb;
-        makeCombination(candidates, target, 0, comb, 0, res);
-        return res;        
+
+    set<vector<int>> ss;
+
+    void getAllCombination(vector<int>& arr,
+                           int idx,
+                           int target,
+                           vector<vector<int>>& ans,
+                           vector<int>& combination)
+    {
+        // target achieved
+        if(target == 0)
+        {
+            if(ss.find(combination) == ss.end())
+            {
+                ans.push_back(combination);
+                ss.insert(combination);
+            }
+            return;
+        }
+
+        // invalid case
+        if(idx == arr.size() || target < 0)
+        {
+            return;
+        }
+
+        combination.push_back(arr[idx]);
+
+        // single take
+        getAllCombination(arr, idx + 1,
+                          target - arr[idx],
+                          ans, combination);
+
+        // multiple take
+        getAllCombination(arr, idx,
+                          target - arr[idx],
+                          ans, combination);
+
+        combination.pop_back();
+
+        // exclude
+        getAllCombination(arr, idx + 1,
+                          target,
+                          ans, combination);
     }
 
-private:
-    void makeCombination(std::vector<int>& candidates, int target, int idx, vector<int>& comb, int total, vector<vector<int>>& res) {
-        if (total == target) {
-            res.push_back(comb);
-            return;
-        }
+    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
 
-        if (total > target || idx >= candidates.size()) {
-            return;
-        }
+        vector<vector<int>> ans;
+        vector<int> combination;
 
-        comb.push_back(candidates[idx]);
-        makeCombination(candidates, target, idx, comb, total + candidates[idx], res);
-        comb.pop_back();
-        makeCombination(candidates, target, idx + 1, comb, total, res);
-    }    
+        getAllCombination(arr, 0, target, ans, combination);
+
+        return ans;
+    }
 };
