a51c9fb9ee06ec520845b4e77bc82ed009db867f
Muzammil Ahmed <155309512+Muzammil4742@users.noreply.github.com>
2026-06-27 22:09:49 +0500

    Time: 0 ms (100.00%) | Memory: 64.8 MB (92.68%) - LeetSync

---
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void calculateSum(TreeNode* root, int low, int high, int &sum)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+
+        if (root->val >= low && root->val <= high)
+        {
+            sum += root->val;
+        }
+
+        calculateSum(root->left, low, high, sum);
+        calculateSum(root->right, low, high, sum);
+    }
+
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int sum = 0;
+        calculateSum(root, low, high, sum);
+        return sum;
+    }
+};
